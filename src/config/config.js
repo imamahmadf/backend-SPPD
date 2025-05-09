2ce3becc623cde4a55d0581b80e9bbcd708117f6
imamahmadf <fahruraziimam22@gmail.comm>
2025-05-09 18:17:19 +0800

    fix migration

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,7 @@ require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 module.exports = {
   development: {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
